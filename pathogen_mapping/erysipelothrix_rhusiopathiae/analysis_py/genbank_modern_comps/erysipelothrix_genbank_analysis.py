576dbf96e3b7cc86f61b6ae67b742ca12112c7c2
Ian Light <86308592+ilight1542@users.noreply.github.com>
2025-01-27 14:21:55 +0100

    removal of unnecessary imports

--- pathogen_mapping/erysipelothrix_rhusiopathiae/analysis_py/genbank_modern_comps/erysipelothrix_genbank_analysis.py
+++ pathogen_mapping/erysipelothrix_rhusiopathiae/analysis_py/genbank_modern_comps/erysipelothrix_genbank_analysis.py
@@ -35,8 +35,6 @@ os.chdir('/Users/ad_loris/Nextcloud/keylab/projects/ak_ancient_zoonosis_project/
 
 sys.path.append("/Users/ad_loris/Documents/key_lab")
 import analysispy_modules_devmain as apy
-import analysispy_modules_main as apy_main
-import analysispy_modules_testdev as apyt
 
 first_run = False
 coverage_based_cutoffs = True
@@ -161,7 +159,7 @@ outgroup_bool = np.in1d(sampleNames , outgroup_name)
 
 unique_samples=np.isin(sampleNames,unique_samples_to_use)
 
-# remove suppressed records from NCBI:
+# remove suppressed records from NCBI: (see accessions_to_suppress.txt)
 pattern = re.compile('(?:GCA_029073425|GCF_029073305|GCA_029102595.1|GCA_029102585.1|GCA_029102545.1|GCA_029102525.1|GCA_029102535.1|GCA_029074585.1|GCA_029074515.1|GCA_029074505.1|GCA_029074475.1|GCA_029074465.1|GCA_029074445.1|GCA_029074425.1|GCA_029074405.1|GCA_029074385.1|GCA_029074345.1|GCA_029074355.1|GCA_029074315.1|GCA_029074285.1|GCA_029074305.1|GCA_029074245.1|GCA_029074265.1|GCA_029074215.1|GCA_029074205.1|GCA_029074185.1|GCA_029074155.1|GCA_029074145.1|GCA_029074095.1|GCA_029074105.1|GCA_029074085.1|GCA_029074065.1|GCA_029074035.1|GCA_029074005.1|GCA_029073995.1|GCA_029073985.1|GCA_029073945.1|GCA_029073955.1|GCA_029073915.1|GCA_029073895.1|GCA_029073885.1|GCA_029073865.1|GCA_029073845.1|GCA_029073815.1|GCA_029073805.1|GCA_029073785.1|GCA_029073765.1|GCA_029073745.1|GCA_029073695.1|GCA_029073715.1|GCA_029073685.1|GCA_029073665.1|GCA_029073645.1|GCA_029073625.1|GCA_029073595.1|GCA_029073585.1|GCA_029073565.1|GCA_029073545.1|GCA_029073525.1|GCA_029073505.1|GCA_029073475.1|GCA_029073455.1|GCA_029073445.1|GCA_029073425.1|GCA_029073405.1|GCA_029073365.1|GCA_029073345.1|GCA_029073355.1|GCA_029073325.1|GCA_029073305.1|GCA_029073265.1|GCA_029073275.1|GCA_029073175.1|GCA_029073165.1|GCA_029073145.1|GCA_029073125.1|GCA_029073105.1|GCA_029073065.1|GCA_029073055.1|GCA_029073045.1|GCA_029073025.1|GCA_029073005.1|GCA_029073235.1|GCA_029073215.1|GCA_029073205.1|GCA_029074545.1|GCA_029074555.1|GCA_029072975.1|GCA_029072965.1|GCA_029072945.1|GCA_029072925.1|GCA_029072905.1|GCA_029072885.1|GCA_029072845.1|GCA_029072765.1|GCA_029072745.1|GCA_029072705.1|GCA_029072715.1|GCA_029072675.1|GCA_029072665.1|GCA_029072645.1|GCA_029072615.1|GCA_029072605.1|GCA_029072555.1|GCA_029072565.1|GCA_029072545.1|GCA_029072525.1|GCA_029072505.1|GCA_029072485.1|GCA_029072465.1|GCA_029072445.1|GCA_029072425.1|GCA_029072405.1|GCA_029072385.1|GCA_029072365.1|GCA_029072345.1|GCA_029072325.1|GCA_029072295.1|GCA_029072285.1|GCA_029072265.1|GCA_029072235.1|GCA_029072225.1|GCA_029072205.1|GCA_029072135.1|GCA_029072155.1|GCA_029072145.1|GCA_029072125.1|GCA_029072095.1|GCA_029072085.1|GCA_029072045.1|GCA_029072035.1|GCA_029072025.1|GCA_029072005.1|GCA_029075055.1|GCA_029075045.1|GCA_029075015.1|GCA_029075005.1|GCA_029074955.1|GCA_029074965.1|GCA_029074945.1|GCA_029074915.1|GCA_029074905.1|GCA_029074885.1|GCA_029074835.1|GCA_029074865.1|GCA_029074825.1|GCA_029074805.1|GCA_029074785.1|GCA_029074765.1|GCA_029074745.1|GCA_029074715.1|GCA_029074705.1|GCA_029074685.1|GCA_029074625.1|GCA_029074635.1|GCA_029074615.1|GCA_029074605.1|GCA_029077745.1|GCA_029077715.1|GCA_029077705.1|GCA_029077685.1|GCA_029077665.1|GCA_029077635.1|GCA_029077625.1|GCA_029077605.1|GCA_029077585.1|GCA_029077565.1|GCA_029077545.1|GCA_029077525.1|GCA_029077505.1|GCA_029077485.1|GCA_029077455.1|GCA_029077445.1|GCA_029077425.1|GCA_029077405.1|GCA_029077385.1|GCA_029072785.1|GCA_029072855.1|GCA_029072805.1|GCA_029072815.1)') 
 suppressed_name = np.array(list(filter(pattern.search, list(sampleNames))))
 suppressed_bool = np.in1d(sampleNames , suppressed_name)
@@ -311,7 +309,7 @@ calls[failed_maf_all]=4
 
 ## Filter per site across samples
 # Ignore here outgroup samples!
-called_100_modern_ingroup=np.where(np.sum(calls[:,((ingroup_modern_bool | outgroup_bool ))]<4,axis=1) >= (np.sum((ingroup_modern_bool | outgroup_bool ))))[0]
+called_100_modern_ingroup_outgroup=np.where(np.sum(calls[:,((ingroup_modern_bool | outgroup_bool ))]<4,axis=1) >= (np.sum((ingroup_modern_bool | outgroup_bool ))))[0]
 
 # ID any sites to set to N in ancient samples
 filter_parameter_site_per_projection = {\
@@ -327,82 +325,6 @@ calls_for_projection=calls[:,ancient_bool]
 calls_for_projection[( projection_failed_quals | projection_failed_maf | projection_failed_cov )] = 4
 calls[:,ancient_bool]=calls_for_projection
 
-
-# %% QC Plotting
-#################################################
-## Covered positions histogram ancient samples ##
-#################################################
-plt.hist(np.sum(coverage[:,ancient_bool]>0,axis=1))
-plt.title('Positions with any coverage across ancient sample')
-plt.xlabel('Number of ancient sample with coverage')
-plt.ylabel('Number of positions')
-plt.savefig('histogram_positions_covered_ancient.png',bbox_inches='tight')
-
-#################################################
-## Called positions histogram ancient samples ##
-#################################################
-plt.hist(np.sum((calls[:,ancient_bool]<4),axis=1))
-plt.title('Positions with call across ancient samples')
-plt.xlabel('Number of ancient sample with call')
-plt.ylabel('Number of positions')
-plt.savefig('histogram_positions_called_ancient.png',bbox_inches='tight')
-
-plt.hist(np.sum((calls[:,ancient_bool][called_100_modern_ingroup]<4),axis=1),bins=85)
-plt.title('100 deletion Modern-ingroup positions with call across ancient sample')
-plt.xlabel('Number of ancient samples with call')
-plt.ylabel('Number of positions')
-plt.savefig('histogram_positions_called_100del_ancient.png',bbox_inches='tight')
-
-
-
-#######################################################
-## Positions covered overlap with modern ref genomes ##
-#######################################################
-# calculate proporiton of positions overlapping with each modern, for each ancient
-summary_table={}
-max_table={}
-for a_index in ancient_idx:
-    ancient_name=sampleNames[a_index]
-    summary_table[ancient_name]={}
-    current_max=-1
-    for m_index in modern_idx:
-        modern_name=sampleNames[m_index]
-        overlapping_positions=np.sum((calls[:,m_index] == calls[:,a_index]) & ( calls[:,a_index] <  4 ))
-        non_overlapping_positions=np.sum((calls[:,m_index] != calls[:,a_index]) & ( calls[:,a_index]<  4 ) &  ( calls[:,m_index] < 4 ))
-        diff_overlap_non_overlap=overlapping_positions-non_overlapping_positions
-        summary_table[ancient_name][modern_name]=(diff_overlap_non_overlap)/np.sum((calls[:,m_index] < 4 ) & ( calls[:,a_index] < 4))
-        if (diff_overlap_non_overlap)/np.sum((calls[:,m_index] < 4 ) & ( calls[:,a_index] < 4)) > current_max:
-            current_max = (diff_overlap_non_overlap)/np.sum((calls[:,m_index] < 4 ) & ( calls[:,a_index] < 4))
-            max_table[ancient_name] = modern_name
-
-summary_table_df=pd.DataFrame(summary_table)
-plt.subplots(figsize=(20,10))
-sns.heatmap(summary_table_df)
-plt.title('(Positions agree - Positions disagree)/Positions called in both')
-plt.savefig('heatmap_positions_agreement.png',bbox_inches='tight')
-
-summary_table={}
-max_table={}
-for a_index in ancient_idx:
-    ancient_name=sampleNames[a_index]
-    summary_table[ancient_name]={}
-    current_max=-1
-    for m_index in modern_idx:
-        modern_name=sampleNames[m_index]
-        overlapping_positions=np.sum((calls[:,m_index] == calls[:,a_index]) & ( calls[:,a_index] <  4 ))
-        non_overlapping_positions=np.sum((calls[:,m_index] != calls[:,a_index]) & ( calls[:,a_index]<  4 ) &  ( calls[:,m_index] < 4 ))
-        diff_overlap_non_overlap=overlapping_positions-non_overlapping_positions
-        summary_table[ancient_name][modern_name]=diff_overlap_non_overlap
-        if (diff_overlap_non_overlap)/np.sum((calls[:,m_index] < 4 ) & ( calls[:,a_index] < 4)) > current_max:
-            current_max = (diff_overlap_non_overlap)/np.sum((calls[:,m_index] < 4 ) & ( calls[:,a_index] < 4))
-            max_table[ancient_name] = modern_name
-
-
-summary_table_df=pd.DataFrame(summary_table)
-plt.subplots(figsize=(20,10))
-sns.heatmap(summary_table_df)
-plt.title('Positions compared')
-
 # %%
 [mutQual, mutQualIsolates] = apy.ana_mutation_quality(calls[:,ingroup_bool],quals[:,ingroup_bool]) # get FQ value for SNP across samples. mutQualIsolates contains sample indices for sample pair FQ based on. 
 mutQual = np.nan_to_num(mutQual, nan=-1) # turn mutQual nan's to -1; necessary to avoid later warning
@@ -415,7 +337,7 @@ ldo050_bool = np.in1d(sampleNames , ldo050_index)
 
 candpos_modern_ldo = np.where( np.sum(hasmutation[:,(~ancient_bool | ldo050_bool)], axis=1)>0  )[0]
 
-goodpos=np.intersect1d(candpos_modern_ldo,called_100_modern_ingroup)
+goodpos=np.intersect1d(candpos_modern_ldo,called_100_modern_ingroup_outgroup)
 
 def get_read_info(qualimap_path):
     with open(qualimap_path) as f:
@@ -496,8 +418,6 @@ refgenome_nts = apy.extract_outgroup_mutation_positions(ref_genome_folder, apy.p
 refgenome_nts_for_tree=refnt[goodpos2useTree]
 
 
-calls_for_tree_anc_ref_no_outgroup = np.concatenate((apy.idx2nts(refgenome_nts_for_tree_anc[:, None]),calls_for_tree_anc_no_outgroup),axis=1) # first column now refgenome_nts; refgenome_nts[:, None] to make ndims (2) same for both
-calls_for_tree_anc_ref_outgroup = np.concatenate((apy.idx2nts(refgenome_nts_for_tree_anc[:, None]),calls_for_tree_anc),axis=1)
 
 calls_for_tree_ref_no_outgroup = np.concatenate((apy.idx2nts(refgenome_nts_for_tree[:, None]),calls_for_tree_no_outgroup),axis=1) # first column now refgenome_nts; refgenome_nts[:, None] to make ndims (2) same for both
 calls_for_tree_ref_outgroup = np.concatenate((apy.idx2nts(refgenome_nts_for_tree[:, None]),calls_for_tree),axis=1)
@@ -506,11 +426,9 @@ treesampleNamesLong_ref_no_outgroup = np.append(['Sref'],treesampleNamesLong_no_
 treesampleNamesLong_ref_outgroup = np.append(['Sref'],treesampleNamesLong)
 
 
-apyt.write_calls_sampleName_to_fasta(calls_for_tree_ref_outgroup,treesampleNamesLong_ref_outgroup,f'100_deletion_modern_goodpos_projected_ancient')
+apy.write_calls_sampleName_to_fasta(calls_for_tree_ref_outgroup,treesampleNamesLong_ref_outgroup,f'100_deletion_modern_goodpos_projected_ancient')
 
-
-apy_main.generate_tree(calls_for_tree_ref_outgroup,treesampleNamesLong_ref_outgroup,treesampleNamesLong_ref_outgroup,refgenome,filetag=f'100_deletion_modern_ldo_goodpos_projected_ancient',buildTree='ML',additional_raxml_parameters=f'--threads 4 --bs-trees 100 --bs-metric TBE --all --redo')
-
+# generate tree on cluster
 
 # %% generate tree just for EASI tree -- allowing for SNVs called in that specific sample
 hasmutation = (calls != refnti_m) & (calls < 4) & (np.tile(mutQual,(1,num_samples)) >= 1) # consider only ingroup samples; mutQual >= 1 is very loose. Important filter with low qual data! refnt not ancnt!!!
@@ -525,7 +443,7 @@ for s_indx in np.where(ancient_bool)[0]:
         subsample_to_use= ~ancient_bool
         subsample_to_use[s_indx]=True
 
-        candpos_modern_100del_called_ancient=np.intersect1d(called_100_ancient_sample,candpos_modern_100del)
+        candpos_modern_100del_called_ancient=np.intersect1d(called_100_ancient_sample,called_100_modern_ingroup_outgroup)
 
         goodpos2useTree=np.intersect1d(candpos,candpos_modern_100del_called_ancient)
 
